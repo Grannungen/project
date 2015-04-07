08081981ad94321f72ebfbf30204cf374d3c79bb
sgotling <simon.gotling@hotmail.com>
2015-04-07 14:29:04 +0200

    Jag har försökt börja med att kunna lägga till ett möte med hjälp av model men det funkar inte som det ska. Controllern hittar inte model

--- app/js/meetingEditorCtrl.js
+++ app/js/meetingEditorCtrl.js
@@ -1,5 +1,21 @@
 meetingAgendaPlanner.controller('meetingEditorCtrl', function ($scope) {
 	$scope.showEditor = false;
+	$scope.hourList = [];
+	this.setHourList = function () {
+		var i = 1;
+		while(i<24){	
+		var time = i.toString();
+		time = time + ":00"
+		$scope.hourList.push(time);
+		i++;
+		}
+		 console.log($scope.hourList)
+	}
+	this.setHourList();
+
+	// var test = meetingModel.test;
+	// alert = test;
+
 	// $scope.mainWindowClicked = function (e) {
 	// 	console.log(e);
 	// 	if($(e).hasClass("activity"))

3e2b2c660b2fe63b69f9cc01b31ad49bc5db3abb
Henning Spett <henningspett@Hennings-MacBook-Pro.local>
2015-05-05 16:26:44 +0200

    some changes to navbar

--- app/js/model.js
+++ app/js/model.js
@@ -233,30 +233,10 @@ meetingAgendaPlanner.factory('meetingAgendaModel', function ($resource) {
 	// }
 	// this is the instance of our main model
 	// this is what you should use in your application
-	return this;
+
+    	return this;
 
 
-}); // model end
-	// var model = new Model();
 
 
-	// you can use this method to create some test data and test your implementation
-	
-	// addActivity(activity, position?)
-	// function createTestData(){
-	// 	model.addDay();
-	// 	model.addActivity(new Activity("Introduction",10,0,""),0);
-	// 	model.addActivity(new Activity("Idea 1",30,0,""),0);
-	// 	model.addActivity(new Activity("Working in groups",35,1,""),0);
-	// 	model.addActivity(new Activity("Idea 1 discussion",15,2,""),0);
-	// 	model.addActivity(new Activity("Coffee break",20,3,""),0);
-		
-	// 	console.log("Day Start: " + model.days[0].getStart());
-	// 	console.log("Day End: " + model.days[0].getEnd());
-	// 	console.log("Day Length: " + model.days[0].getTotalLength() + " min");
-	// 	$.each(ActivityType,function(index,type){
-	// 		console.log("Day '" + ActivityType[index] + "' Length: " +  model.days[0].getLengthByType(index) + " min");
-	// 	});
-	// 	console.log(model.days)
-	// }
-	// createTestData();
+}); 
--- app/js/weekViewCtrl.js
+++ app/js/weekViewCtrl.js
@@ -1,6 +1,19 @@
-meetingAgendaPlanner.controller('weekViewCtrl', function($scope, meetingAgendaModel, $location){
+meetingAgendaPlanner.controller('weekViewCtrl', function($scope, meetingAgendaModel, $location, $resource){
 	$scope.wholeHourList = [];
 	//$scope.dataElement = $(".dataElement");
+	//this.Dish = $resource('http://www.google.com/calendar/feeds/developer-calendar@google.com/public/full?alt=json-in-script&callback=insertAgenda&orderby=starttime&max-results=15&singleevents=true&sortorder=ascending&futureevents=true');
+	//console.log(Dish);
+	this.Dish = $resource("https://www.google.com/calendar/render#main_7");
+	console.log(this.Dish);
+	//$.ajax({
+      //      type: "GET",
+        //    dataType: 'json',
+          //  cache: false,
+          //  url: url,
+           // success: function (data) {
+ 	//				console.log(data);
+      //          }
+       // });
 	$scope.addNew = function () {
 		var target = event.target.id;
 		var chosenDayTime = target.split(",");

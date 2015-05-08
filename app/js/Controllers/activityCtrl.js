ccce3fbe446cda5478acace86a19bce1327b10d4
sgotling <sgotling>
2015-05-08 16:51:27 +0200

    Felhantering vid inmatning. Cancelknapp i popUps

--- app/js/Controllers/activityCtrl.js
+++ app/js/Controllers/activityCtrl.js
@@ -25,21 +25,57 @@ meetingAgendaPlanner.controller('activityCtrl', function ($scope, $rootScope, me
 	}
 
 
+	var testInput = function () {
+		console.log("$scope.activityCtrlGlobal.ActivityName: " + $scope.activityCtrlGlobal.ActivityName)
+		if($scope.activityCtrlGlobal.showActivityPopUpNew==true){
+			if($scope.activityCtrlGlobal.ActivityName==undefined || $scope.activityCtrlGlobal.ActivityName==""){
+			alert("Please enter a name for the activity.")
+			return false
+			}
+			if($scope.activityCtrlGlobal.length==undefined){
+				alert("Please enter the time length of the activity.")
+				return false
+			}
+		}
+		if($scope.activityCtrlGlobal.showActivityPopUpEdit==true){
+			if($scope.activityCtrlGlobal.selectedActivityName==undefined || $scope.activityCtrlGlobal.selectedActivityName==""){
+			alert("Please enter a name for the activity.")
+			return false
+			}
+			if($scope.activityCtrlGlobal.selectedActivityLength==undefined){
+				alert("Please enter the time length of the activity.")
+				return false
+			}		
+		}
+
+
+	}
 	// Add activities to day by using addAvtivity(activity, day, position)
+
 	$scope.addNewActivity = function(){
-		
-		var Activity = meetingAgendaModel.Activity;
-		var ActivityType = meetingAgendaModel.ActivityType;
-		$rootScope.meetingCtrlGlobal.selectedDay._addActivity(new Activity($scope.activityCtrlGlobal.ActivityName,$scope.activityCtrlGlobal.length,$scope.activityCtrlGlobal.typeId,$scope.activityCtrlGlobal.description),$scope.dayIndex);
+		var bool = testInput()
+		if(bool !=false){
+			$rootScope.activityCtrlGlobal.showActivityPopUpNew=false
+			var Activity = meetingAgendaModel.Activity;
+			var ActivityType = meetingAgendaModel.ActivityType;
+			$rootScope.meetingCtrlGlobal.selectedDay._addActivity(new Activity($scope.activityCtrlGlobal.ActivityName,$scope.activityCtrlGlobal.length,$scope.activityCtrlGlobal.typeId,$scope.activityCtrlGlobal.description),$scope.dayIndex);
 			$.each(ActivityType,function(index,type){
 			// console.log("Day '" + ActivityType[index] + "' Length: " +  meetingAgendaModel.days[0].getLengthByType(index) + " min");
 			 });
+
+		}
+		
 	}
 	$scope.changeActivity = function () {
-		var activity = meetingAgendaModel.selectedActivity;
-		activity.setName($rootScope.activityCtrlGlobal.selectedActivityName);
-		activity.setLength($rootScope.activityCtrlGlobal.selectedActivityLength);
-		activity.setTypeId($scope.activityCtrlGlobal.typeId);
-		activity.setDescription($rootScope.activityCtrlGlobal.selectedActivityDescription);
+		var bool = testInput();
+		if(bool !=false){
+			$rootScope.activityCtrlGlobal.showActivityPopUpEdit=false;
+			var activity = meetingAgendaModel.selectedActivity;
+			activity.setName($rootScope.activityCtrlGlobal.selectedActivityName);
+			activity.setLength($rootScope.activityCtrlGlobal.selectedActivityLength);
+			activity.setTypeId($scope.activityCtrlGlobal.typeId);
+			activity.setDescription($rootScope.activityCtrlGlobal.selectedActivityDescription);
+		}
+		
 	}
 });
